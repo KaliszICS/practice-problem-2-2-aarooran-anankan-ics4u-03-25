af25f40f81420805ad2d41c2211db2e6d85773b3
aarooran-anankan-ics4u-03-25 <aarooran.anankan@student.tdsb.on.ca>
2025-04-02 19:09:52 +0000

    Final Assignment

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -1,4 +1,3 @@
-
 class Notes {
 	public static void main(String[] args) {
 		//Binary Search
@@ -9,7 +8,7 @@ class Notes {
 		int num = 234; // number we're looking for
 		int index = -1;
 
-		Arrays.sort(numArray);
+		//Arrays.sort(numArray);
 
 		for (int i = 0; i < numArray.length; i++) {
 			System.out.print(numArray[i] + " ");
--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,82 @@
-public class PracticeProblem {
+import java.util.Arrays;
 
-	public static void main(String args[]) {
+public class PracticeProblem {
+	public static void main(String args[]) {}
+
+	public static int[] compareSearch(int[] array, int number) {
+
+		Arrays.sort(array);
+
+		int[] ansArr = {0, 0};
 		
+		for (int i = 0; i < array.length; i++) { //Seq
+			ansArr[0] = i+1;
+			if (array[i] == number) {
+				i = array.length;
+			}
+		}
+
+		int low = 0;
+		int high = array.length - 1;
+		int mid;
+
+		while(low <= high) { //bin
+
+			ansArr[1]++;
+
+			mid = (high + low) / 2;
+
+			if (number == array[mid]) {
+				//ansArr[1] = mid;	
+				low = high + 1;
+			}
+			else if (number > array[mid]) {
+				low = mid + 1;
+			}
+			else {
+				high = mid - 1;
+			}
+		}
+
+		return ansArr;
 	}
-	
+
+	public static int[] compareStringSearch(String[] array, String word) {
+
+		Arrays.sort(array);
+
+		int[] ansArr = {0, 0};
+		
+		for (int i = 0; i < array.length; i++) { //Seq
+			ansArr[0] = i+1;
+			if (array[i].compareTo(word)==0) {
+				i = array.length;
+			}
+		}
+
+		int low = 0;
+		int high = array.length - 1;
+		int mid;
+
+		while(low <= high) { //bin
+
+			ansArr[1]++;
+
+			mid = (high + low) / 2;
+
+			if (word.compareTo(array[mid])==0) {
+				//ansArr[1] = mid;	
+				low = high + 1;
+			}
+			else if (word.compareTo(array[mid])>0) {
+				low = mid + 1;
+			}
+			else {
+				high = mid - 1;
+			}
+		}
+
+		return ansArr;
+	}
+
 }
